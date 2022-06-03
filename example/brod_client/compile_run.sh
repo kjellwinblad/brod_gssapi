035581dc20d3c0284dd73a34203517f7f0ea1bb6
Kjell Winblad <kjellwinblad@gmail.com>
2022-06-03 15:22:45 +0200

    fix: make sure everything gets built

--- example/brod_client/compile_run.sh
+++ example/brod_client/compile_run.sh
@@ -2,15 +2,8 @@
 
 #sleep infinity
 
-
-# docker-compose build
-
-# ./setup-test-env.sh
-sleep 7
 rebar3 clean
 rebar3 compile
 
 erl -noshell `rebar3 path | xargs -n1 echo -pa | tr '\n' ' '` -eval 'example:main([])'
 
-
-# docker-compose down
